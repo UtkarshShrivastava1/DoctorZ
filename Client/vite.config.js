07b01c89ca7068cebe2e5671afba62b2865c37ea
Sujal Raj <sujalraj2064@gmail.com>
2025-09-03 17:08:31 +0530

    vite config

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -5,10 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  //    server: {
-  //   proxy: {
-  //     "/api": "http://localhost:3000/",
-  //   },
-  // },
-})
+  base: "./",
+  build: {
+    outDir: "dist"
+  }
+});
+
